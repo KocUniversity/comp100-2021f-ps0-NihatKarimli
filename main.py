31e23a9caa1186418c90a5dfb39fce9861113795
NIHAT KERIMLI <91715901+NihatKarimli@users.noreply.github.com>
2021-10-01 06:29:09 +0000

    Implemented homework for 1st lab class

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+print('Enter number x:')
+x = int(input())
+print('Enter number y:')
+y = int(input())
+z = x**y
+print('Result, ',+ z)
+print ('log2(x)', math.log2(x))                        
+print ('Student ID: 79117')               
